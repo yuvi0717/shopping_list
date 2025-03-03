555b9147bdf8c17923cecec3bd64f1d85c9cfc15
Yuvraj Sen <yuvisen001@gmail.com>
2025-03-03 23:41:31 +0530

    sending http request

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-
+import 'package:http/http.dart' as http; 
 import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/category.dart';
 
